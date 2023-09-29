a1c036155531dbb5479085ecf9e98a5ab72c8613
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2023-09-29 16:36:38 +0100

    Create setup function

--- hangman.py
+++ hangman.py
@@ -1,2 +1,30 @@
+'''
+Simple hangman game
+'''
+
+from random import choice
+
+
+def setup():
+    '''
+    Game settings
+    Returns a random word and the initial number of tries
+    '''
+    word: str = choice(['bottle', 'screen', 'banana', 'building', 'raspberry', 'house'])
+    tries: int = 5
+
+    username: str = input('What is your name? >> ')
+    print(f'Welcome to hangman, {username}!')
+
+    data: dict = {
+        'word': word,
+        'tries': tries
+    }
+    return data
+
+
 def run_game():
+    '''
+    Main game loop
+    '''
     pass
--- test_hangman.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-class HangmanTest(unittest.TestCase):
-    pass
-
-
-if __name__ == '__main__':
-    unittest.main()
